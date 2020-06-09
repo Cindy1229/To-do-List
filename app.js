c077ce7a3206107d992f8dcd1487a33c459a34e0
Cindy1229 <leticiashi3@gmail.com>
2020-06-09 12:36:10 -0400

    Cpmmit

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ const dateElement=document.querySelector(".date");
 const list=document.getElementById("list");
 const input=document.getElementById("input");
 
+//List to store to-dos
+let LIST=[];
+let id=0;
+
+const CHECK="check-circle";
+const UNCHECK="uncheck-circle";
+const LINE_THROUGH="lineThrough";
+
+
 //Add the todo text to the list
-function addToDo(toDo) {
+function addToDo(toDo, id, done, trash) {
+  if(trash){return;}
+
+  const DONE=done?CHECK:UNCHECK;
+  const LINE=done?LINE_THROUGH:"";
+
   const text=`<li class="item">
-                <i class="check" job="complete"></i>
-                <p class="text">${toDo}</p>
-                <i class="trash" job="delete"></i>
+                <i class="fa ${DONE} complete" job="complete" id="${id}"></i>
+                <p class="text ${LINE}">${toDo}</p>
+                <i class="fa trash delete" job="delete" id="${id}"></i>
               </li>`
   const position="beforeend";
   list.insertAdjacentHTML(position, text);
@@ -21,9 +35,27 @@ document.addEventListener('keyup', function(e) {
     const toDo=input.value;
     //Add the input text to the list
     if (toDo) {
-      addToDo(toDo)
+      addToDo(toDo, id, false, false);
+      LIST.push(
+        {
+          name: toDo,
+          id: id,
+          done: false,
+          trash: false
+        }
+      );
+      id++;
+      //Empty the input
+      input.value="";
     }
-    //Empty the input
-    input.value="";
   }
 });
+
+//Complete todo item
+function completeToDo(element) {
+  element.toggle(CHECK);
+  element.toggle(UNCHECK);
+
+
+
+}
