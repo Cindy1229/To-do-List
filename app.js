3b441b97cf40b0936cb5ad144864f46249fd6b7c
Cindy1229 <leticiashi3@gmail.com>
2020-06-09 13:19:00 -0400

    commit

--- app.js
+++ app.js
@@ -3,13 +3,36 @@ const dateElement=document.querySelector(".date");
 const list=document.getElementById("list");
 const input=document.getElementById("input");
 
-//List to store to-dos
-let LIST=[];
-let id=0;
-
 const CHECK="check-circle";
 const UNCHECK="uncheck-circle";
 const LINE_THROUGH="lineThrough";
+
+//List to store to-dos
+let LIST, id;
+//Restore data from localStorage
+let data=localStorage.getItem("TODO");
+if (data) {
+  LIST=JSON.parse(data);
+  //Load the LIST to the page
+  loadToDo(LIST);
+  id=LIST.length;
+} else {
+  LIST=[];
+  id=0;
+}
+
+//Load
+function loadToDo(array){
+  array.forEach(function(item) {
+    addToDo(item.name, item.id, item.done, item.trash);
+  });
+
+}
+
+//clear
+clear.addEventListener('click', function() {
+  localStorage.clear();
+});
 
 
 //Add the todo text to the list
@@ -55,7 +78,27 @@ document.addEventListener('keyup', function(e) {
 function completeToDo(element) {
   element.toggle(CHECK);
   element.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
 
-
+  LIST[element.id].done=LIST[element.id].done?false:true;
 
 }
+
+//Remove the to-do item
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+  LIST[element.id].trash=true;
+}
+
+//Target any list elements dynamically
+list.addEventListener('click', function(e) {
+  const elementJOB=e.target.attributes.job.value;
+  //Update local storage
+  localStorage.setItem("TODO", JSON.stringify(LIST));
+
+  if (elementJOB=="complete") {
+    completeToDo(e.target);
+  } else if(elementJOB=="delete"){
+    removeToDo(e.target);
+  }
+});
